e09f1bad10b7adf1ac6424f28e8bcb3e99022eae
Luana Kimley <73035581+luanakimley@users.noreply.github.com>
2021-11-17 12:08:38 +0000

    commit

--- src/ShiftCipher.java
+++ src/ShiftCipher.java
@@ -1,13 +1,61 @@
-// Reference: https://www.baeldung.com/java-caesar-cipher
 public class ShiftCipher {
-    public String decrypt(String text, int key) {
+    // Fields
+    private int key;
+    private String encryptedText;
+    private String decryptedText;
+
+    // Constructors
+
+    // for ShiftCipher with known key
+    public ShiftCipher(int key, String encryptedText) {
+        this.key = key;
+        this.encryptedText = encryptedText;
+    }
+
+    // for ShiftCipher with unknown key
+    public ShiftCipher(String encryptedText) {
+        this.encryptedText = encryptedText;
+    }
+
+    // Getters & Setters
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getEncryptedText() {
+        return encryptedText;
+    }
+
+    public void setEncryptedText(String encryptedText) {
+        this.encryptedText = encryptedText;
+    }
+
+    public String getDecryptedText() {
+        return decryptedText;
+    }
+
+    public void setDecryptedText(String decryptedText) {
+        this.decryptedText = decryptedText;
+    }
+
+    // Decrypt with known key
+    public void decryptKnownKey() {
         StringBuilder result = new StringBuilder();
-        text = text.toUpperCase();
+        this.encryptedText = this.encryptedText.toUpperCase();
 
-        int shift = 26 - (key % 26);
+        /*
+         * use 26 - key instead of using negative keys so that it won't have a problem when looping back
+         * use '% 26' so shift won't be negative
+         */
 
-        for (char c : text.toCharArray()) {
-            if (c != ' ') {
+        int shift = 26 - (this.key % 26);
+
+        for (char c : this.encryptedText.toCharArray()) {
+            if (c != ' ') { // to preserve space characters
                 result.append((char) (((int) c + shift - 'A') % 26 + 'A'));
             }
             else {
@@ -15,18 +63,25 @@ public class ShiftCipher {
             }
         }
 
-        return result.toString();
+        this.decryptedText = result.toString();
     }
 
-    public String decryptExhaustiveKeySearch(String message) {
+    // Decrypt with exhaustive key search
+    public void decryptExhaustiveKeySearch(String word) {
         String result = "";
-
+        int tempKey = 0;
+        word = word.toUpperCase();
         for (int i=1; i <= 26; i++) {
-            String temp = decrypt(message, i);
-            if (temp.contains("DONE"))
+            this.key = i;
+            decryptKnownKey();
+            String temp = this.decryptedText;
+            if (temp.contains(word)) {
+                tempKey = i;
                 result = temp;
+            }
         }
-
-        return result;
+        this.key = tempKey;
+        this.decryptedText = result;
     }
+
 }
