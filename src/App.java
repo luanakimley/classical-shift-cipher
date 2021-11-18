bc3c249ba71fa93a776e6d6104fffcaf3b1dda4f
Luana Kimley <73035581+luanakimley@users.noreply.github.com>
2021-11-18 12:07:40 +0000

    commit

--- src/App.java
+++ src/App.java
@@ -8,15 +8,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         App app = new App();
-        app.start();
-
+        app.menu();
     }
 
-    public void start() {
-        menu();
-    }
-
-    public static void menu() {
+    public void menu() {
             displayMenu();
             try {
                 int option = inputValidInt(1, 5, "Input option: ", "Invalid menu option");
@@ -47,28 +42,31 @@ public class App {
         }
 
         public static void displayMenu() {
-            System.out.println("**** MENU ****");
+            System.out.println("\n**** CLASSICAL SHIFT CIPHER MENU ****");
             System.out.println("** OPTIONS **");
-            System.out.println("1. Decrypt message\n" +
-                    "2. Decrypt message exhaustive key\n"+
+            System.out.println("1. Decrypt message from text file\n" +
+                    "2. Decrypt message exhaustive key from text file\n"+
                     "3. Decrypt your own message\n"+
                     "4. Decrypt your own message using exhaustive key\n" +
-                    "5. Exit");
+                    "5. Quit");
         }
 
         public static void menuDecrypt() {
-            System.out.println("**** DECRYPT MESSAGE ****");
-            String fileName = "ciphertext.txt";
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("\n**** DECRYPT MESSAGE ****");
+            System.out.println("Enter file name:");
+            String fileName = keyboard.nextLine();
             try {
                 File file = new File(fileName);
                 Scanner input = new Scanner(file);
 
-                String ciphertext1 = input.nextLine();
-                ShiftCipher shiftCipher = new ShiftCipher(3, ciphertext1);
+                String ciphertext = input.nextLine();
+                ShiftCipher shiftCipher = new ShiftCipher(3, ciphertext);
 
-                System.out.println("Cipher text 1: " + ciphertext1);
+                System.out.println("Cipher text: " + ciphertext);
                 shiftCipher.decryptKnownKey();
                 System.out.println(shiftCipher.getDecryptedText());
+                System.out.println();
 
             } catch (NoSuchElementException e) {
                 System.out.println("Your input must include 2 lines of cipher text.");
@@ -77,20 +75,23 @@ public class App {
             }
         }
         public static void menuDecryptExhaustiveKeySearch(){
-            System.out.println("**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
-            String fileName = "ciphertext.txt";
+            System.out.println("\n**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter file name:");
+            String fileName = keyboard.nextLine();
             try {
                 File file = new File(fileName);
                 Scanner input = new Scanner(file);
 
                 input.nextLine();
-                String ciphertext2 = input.nextLine();
-                ShiftCipher shiftCipher = new ShiftCipher(ciphertext2);
+                String ciphertext = input.nextLine();
+                ShiftCipher shiftCipher = new ShiftCipher(ciphertext);
 
-                System.out.println("Cipher text 2: " + ciphertext2);
+                System.out.println("Cipher text: " + ciphertext);
                 shiftCipher.decryptExhaustiveKeySearch("done");
                 System.out.println("Key: " + shiftCipher.getKey());
                 System.out.println(shiftCipher.getDecryptedText());
+                System.out.println();
             } catch (NoSuchElementException e) {
                 System.out.println("Your input must include 2 lines of cipher text.");
             } catch (FileNotFoundException e) {
@@ -98,37 +99,36 @@ public class App {
             }
         }
         public static void decryptYourOwnMessage(){
-            System.out.println("**** DECRYPT YOUR OWN MESSAGE ****");
-            try {
-                Scanner keyboard = new Scanner(System.in);
-                System.out.println("Enter a message you wish to decrypt: ");
-                String ciphertext3 = keyboard.nextLine();
+            System.out.println("\n**** DECRYPT YOUR OWN MESSAGE ****");
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter a message you wish to decrypt: ");
+            String ciphertext = keyboard.nextLine();
+            System.out.println("Enter key: ");
+            int key = keyboard.nextInt();
 
-                ShiftCipher shiftCipher = new ShiftCipher(ciphertext3);
+            ShiftCipher shiftCipher = new ShiftCipher(key, ciphertext);
 
-                System.out.println("Cipher text 3-your own: " + ciphertext3);
-                shiftCipher.decryptKnownKey();
-                System.out.println(shiftCipher.getDecryptedText());
-            } catch (NoSuchElementException e) {
-                System.out.println("Your input must include 2 lines of cipher text.");
-            }
+            System.out.println("Cipher text - your own: " + ciphertext);
+            shiftCipher.decryptKnownKey();
+            System.out.println(shiftCipher.getDecryptedText());
+            System.out.println();
     }
 
     public static void decryptYourOwnMessageUsingExhaustiveKey() {
-        System.out.println("**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
+        System.out.println("\n**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
 
-        try {
-            Scanner keyboard = new Scanner(System.in);
-            System.out.println("Enter a message you wish to decrypt using exhaustive key: ");
-            String ciphertext4 = keyboard.nextLine();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a message you wish to decrypt using exhaustive key: ");
+        String ciphertext = keyboard.nextLine();
+        System.out.println("Enter a word that is inside your message: ");
+        String word = keyboard.nextLine();
 
-            ShiftCipher shiftCipher = new ShiftCipher(ciphertext4);
-            System.out.println("Cipher text 4: " + ciphertext4);
-            shiftCipher.decryptExhaustiveKeySearch("");
-            System.out.println(shiftCipher.getDecryptedText());
-        } catch (NoSuchElementException e) {
-            System.out.println("Your input must include 2 lines of cipher text.");
-        }
+        ShiftCipher shiftCipher = new ShiftCipher(ciphertext);
+        System.out.println("Cipher text: " + ciphertext);
+        shiftCipher.decryptExhaustiveKeySearch(word);
+        System.out.println("Key: " + shiftCipher.getKey());
+        System.out.println(shiftCipher.getDecryptedText());
+
     }
 
         public static int inputValidInt ( int min, int max, String prompt, String errorMessage)
