3eeeee775d5e8daf0dc2b92f6eab96f4ce750d7d
Luana Kimley <73035581+luanakimley@users.noreply.github.com>
2021-11-19 16:11:32 +0000

    modified Menu

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.menu();
+        menu.mainMenu();
     }
 }
 
--- src/Menu.java
+++ src/Menu.java
@@ -5,126 +5,165 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Menu {
-    public void menu() {
-        displayMenu();
+    public void mainMenu() {
+        displayMainMenu();
         try {
-            int option = inputValidInt(1, 5, "Input option: ", "Invalid menu option");
+            int option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
 
-            while (option != 5) {
+            while (option != 3) {
                 switch (option) {
                     case 1:
-                        menuDecrypt();
+                        menuDecryptFromFile();
                         break;
                     case 2:
-                        menuDecryptExhaustiveKeySearch();
-                        break;
-                    case 3:
-                        decryptYourOwnMessage();
-                        break;
-                    case 4:
-                        decryptYourOwnMessageUsingExhaustiveKey();
+                        menuDecryptOwnMessage();
                         break;
                 }
-                displayMenu();
-                option = inputValidInt(1, 5, "Input option: ", "Invalid menu option");
+                displayMainMenu();
+                option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
             }
             System.out.println("Program ending - thank you");
         } catch (InputMismatchException e) {
             System.out.println("Please enter a number!\n");
-            displayMenu();
+            mainMenu();
         }
     }
 
-    public void displayMenu() {
+    public void displayMainMenu() {
         System.out.println("\n**** CLASSICAL SHIFT CIPHER MENU ****");
         System.out.println("** OPTIONS **");
         System.out.println("1. Decrypt message from text file\n" +
-                "2. Decrypt message exhaustive key from text file\n"+
-                "3. Decrypt your own message\n"+
-                "4. Decrypt your own message using exhaustive key\n" +
-                "5. Quit");
+                "2. Decrypt your own message\n" +
+                "3. Quit");
     }
 
-    public void menuDecrypt() {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("\n**** DECRYPT MESSAGE ****");
-        System.out.println("Enter file name:");
-        String fileName = keyboard.nextLine();
-        try {
-            File file = new File(fileName);
-            Scanner input = new Scanner(file);
-
-            String ciphertext = input.nextLine();
-            ShiftCipher shiftCipher = new ShiftCipher(3, ciphertext);
-
-            System.out.println("Cipher text: " + ciphertext);
-            shiftCipher.decryptKnownKey();
-            System.out.println(shiftCipher.getDecryptedText());
-            System.out.println();
-
-        } catch (NoSuchElementException e) {
-            System.out.println("Your input must include 2 lines of cipher text.");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
-    public void menuDecryptExhaustiveKeySearch(){
-        System.out.println("\n**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter file name:");
-        String fileName = keyboard.nextLine();
-        try {
-            File file = new File(fileName);
-            Scanner input = new Scanner(file);
-
-            input.nextLine();
-            String ciphertext = input.nextLine();
-            ShiftCipher shiftCipher = new ShiftCipher(ciphertext);
-
-            System.out.println("Cipher text: " + ciphertext);
-            shiftCipher.decryptExhaustiveKeySearch("done");
-            System.out.println("Key: " + shiftCipher.getKey());
-            System.out.println(shiftCipher.getDecryptedText());
-            System.out.println();
-
-        } catch (NoSuchElementException e) {
-            System.out.println("Your input must include 2 lines of cipher text.");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
-    public void decryptYourOwnMessage(){
-        System.out.println("\n**** DECRYPT YOUR OWN MESSAGE ****");
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a message you wish to decrypt: ");
-        String ciphertext = keyboard.nextLine();
-        System.out.println("Enter key: ");
-        int key = keyboard.nextInt();
-
-        ShiftCipher shiftCipher = new ShiftCipher(key, ciphertext);
-
-        System.out.println("Cipher text - your own: " + ciphertext);
-        shiftCipher.decryptKnownKey();
-        System.out.println(shiftCipher.getDecryptedText());
+    public void displayDecryptFromFileMenu() {
         System.out.println();
+        System.out.println("**** DECRYPT MESSAGE FROM TEXT FILE ****");
+        System.out.println("Text file must contain two lines of cipher text");
+        System.out.println("Line 1 of text file will contain cipher text that needs to be decrypted using a known key");
+        System.out.println("Line 2 of text file will contain cipher text that needs to be decrypted using exhaustive search");
     }
 
-    public void decryptYourOwnMessageUsingExhaustiveKey() {
-        System.out.println("\n**** DECRYPT EXHAUSTIVE KEY SEARCH MESSAGE ****");
+    public void displayDecryptFromFileMenuOptions() {
+        System.out.println();
+        System.out.println("**** DECRYPT MESSAGE FROM TEXT FILE ****");
+        System.out.println("** OPTIONS **");
+        System.out.println("1. Decrypt using known key");
+        System.out.println("2. Decrypt using exhaustive search");
+        System.out.println("3. Back to main menu");
+    }
 
+    public void displayDecryptOwnMessageMenu() {
+        System.out.println();
+        System.out.println("**** DECRYPT YOUR OWN MESSAGE ****");
+        System.out.println("** OPTIONS **");
+        System.out.println("1. Decrypt using known key");
+        System.out.println("2. Decrypt using exhaustive search");
+        System.out.println("3. Back to main menu");
+    }
+
+
+    public void menuDecryptFromFile() {
+        displayDecryptFromFileMenu();
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter a message you wish to decrypt using exhaustive key: ");
-        String ciphertext = keyboard.nextLine();
-        System.out.println("Enter a word that is inside your message: ");
-        String word = keyboard.nextLine();
+        System.out.println("Enter file name:");
+        String fileName = keyboard.nextLine();
+        try {
+            File file = new File(fileName);
+            Scanner input = new Scanner(file);
 
-        ShiftCipher shiftCipher = new ShiftCipher(ciphertext);
-        System.out.println("Cipher text: " + ciphertext);
-        shiftCipher.decryptExhaustiveKeySearch(word);
-        System.out.println("Key: " + shiftCipher.getKey());
-        System.out.println(shiftCipher.getDecryptedText());
+            String ciphertext1 = input.nextLine();
+            String ciphertext2 = input.nextLine();
 
+            menuDecryptFromFileOptions(ciphertext1, ciphertext2);
+        } catch (NoSuchElementException e) {
+            System.out.println("Your file must include 2 lines of cipher text.");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            mainMenu();
+        }
     }
+
+    public void menuDecryptFromFileOptions(String ct1, String ct2) {
+        displayDecryptFromFileMenuOptions();
+
+        try {
+            Scanner keyboard = new Scanner(System.in);
+            int option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
+
+            while (option != 3) {
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter key: ");
+                        int key = keyboard.nextInt();
+                        ShiftCipher sc1 = new ShiftCipher(key, ct1);
+                        sc1.decryptKnownKey();
+                        System.out.println("Cipher text: " + sc1.getCipherText());
+                        System.out.println("Decrypted text: " + sc1.getDecryptedText());
+                        break;
+                    case 2:
+                        ShiftCipher sc2 = new ShiftCipher(ct2);
+                        System.out.println("Enter a word that the decrypted cipher text will contain: ");
+                        String word = keyboard.next();
+                        sc2.decryptExhaustiveKeySearch(word);
+                        System.out.println("Cipher text: " + sc2.getCipherText());
+                        System.out.println("Discovered key: " + sc2.getKey());
+                        System.out.println("Decrypted text: " + sc2.getDecryptedText());
+                        break;
+                }
+                displayDecryptFromFileMenuOptions();
+                option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
+            }
+            System.out.println("Going back to main menu...");
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number!");
+            menuDecryptFromFileOptions(ct1, ct2);
+        }
+    }
+
+    public void menuDecryptOwnMessage() {
+        displayDecryptOwnMessageMenu();
+
+        try {
+            Scanner keyboard = new Scanner(System.in);
+            int option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
+
+            while (option != 3) {
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter cipher text:");
+                        String ciphertext1 = keyboard.nextLine();
+                        System.out.println("Enter key:");
+                        int key = keyboard.nextInt();
+                        ShiftCipher sc1 = new ShiftCipher(key, ciphertext1);
+                        sc1.decryptKnownKey();
+                        System.out.println("Cipher text: " + sc1.getCipherText());
+                        System.out.println("Decrypted text: " + sc1.getDecryptedText());
+                        break;
+                    case 2:
+                        System.out.println("Enter cipher text:");
+                        String ciphertext2 = keyboard.nextLine();
+                        ShiftCipher sc2 = new ShiftCipher(ciphertext2);
+                        System.out.println("Enter word that the decrypted cipher text will contain:");
+                        String word = keyboard.nextLine();
+                        sc2.decryptExhaustiveKeySearch(word);
+                        System.out.println("Cipher text: " + sc2.getCipherText());
+                        System.out.println("Discovered key: " + sc2.getKey());
+                        System.out.println("Decrypted text: " + sc2.getDecryptedText());
+                        break;
+                }
+                displayDecryptOwnMessageMenu();
+                option = inputValidInt(1, 3, "Input option: ", "Invalid menu option");
+            }
+            System.out.println("Going back to main menu...");
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number!");
+            menuDecryptOwnMessage();
+        }
+    }
+
+
 
     /**
      * This method allows the user to input a number and checks if the number is
